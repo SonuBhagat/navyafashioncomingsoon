cba731480479003d86799e34f471348f9febdaa4
SonuBhagat <bhagatji126@gmail.coml>
2025-07-12 23:35:26 +0530

    schema added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
+import Head from "next/head"
 import { Inter, Playfair_Display } from "next/font/google"
 import "./globals.css"
 
@@ -28,8 +29,35 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
-    generator: 'v0.dev'
+  generator: 'v0.dev'
 }
+
+const schema = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "Navya Fashion",
+  "slogan": "Your Vision, Our Creation",
+  "founder": {
+    "@type": "Person",
+    "name": "Chandni Singh"
+  },
+  "contactPoint": {
+    "@type": "ContactPoint",
+    "telephone": "+91-9326123594",
+    "email": "navya8052023@gmail.com",
+    "contactType": "customer service"
+  },
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "F1323 Bhairav Residency, Kanakia Road, Beverly Park, Next to commissioner bunglow",
+    "addressLocality": "Mira Road East",
+    "postalCode": "401107",
+    "addressCountry": "IN"
+  },
+  "description": "Custom fashion design company specializing in personalized clothing and accessories",
+  "industry": "Fashion Design",
+  "url": "https://navyafashion.com"
+};
 
 export default function RootLayout({
   children,
@@ -38,6 +66,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
